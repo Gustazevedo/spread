683a353eaa33b51f3ce0647f002c2b06f72dd98a
Gustazevedo <46764135+Gustazevedo@users.noreply.github.com>
2019-04-23 20:36:25 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,9 +1,6 @@
-window.onload = function() {
-    document.querySelector(".div_modal").style.display = "none";
-};
-
 function apareceModal() {
-    document.querySelector(".div_modal").style.display = "block";
+    document.querySelector(".div_modal").style.visibility = "visible";
+    document.querySelector(".div_modal").style.opacity = "1";
     document.querySelector(".bg-img").style.opacity = "0.3";
     
 }
